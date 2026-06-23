f21231c55e25d5b17c64ad3d38e09c4ba7de454e
dheeraj1566 <dheerajjangid013@gmail.com>
2026-06-23 17:34:00 +0530

    employee apis added

--- frontend/src/api/clients.js
+++
@@ -1,7 +0,0 @@
-import api from "./axios";
-
-export const fetchClients = (params) => api.get("/clients", { params });
-export const fetchClient = (clientId) => api.get(`/clients/${clientId}`);
-export const createClient = (data) => api.post("/clients", data);
-export const updateClient = (clientId, data) => api.patch(`/clients/${clientId}`, data);
-export const deleteClient = (clientId) => api.delete(`/clients/${clientId}`);
--- frontend/src/api/reportClients.js
+++
@@ -1,7 +0,0 @@
-import api from "./axios";
-
-export const fetchReportClients = () => api.get("/report-clients");
-export const fetchReportClient = (id) => api.get(`/report-clients/${id}`);
-export const createReportClient = (data) => api.post("/report-clients", data);
-export const updateReportClient = (id, data) => api.patch(`/report-clients/${id}`, data);
-export const deleteReportClient = (id) => api.delete(`/report-clients/${id}`);
